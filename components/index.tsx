b0ce034d8dec4c62e8561462aa284b4bf309505d
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-24 10:18:51 -0300

    feat: create price alert card

---
+++ components/index.tsx
@@ -0,0 +1,3 @@
+import PriceAlert from './PriceAlert';
+
+export {PriceAlert};
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,18 +1,176 @@
-import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+  ImageSourcePropType,
+  Image,
+  ImageBackground,
+} from 'react-native';
 
-import {useNavigation} from '@react-navigation/native';
+import {dummyData, COLORS, SIZES, FONTS, icons, images} from '../constants';
+
+// import {useNavigation} from '@react-navigation/native';
+
+import {PriceAlert} from '../components';
+
+interface PropsItem {
+  id: number;
+  image: ImageSourcePropType;
+  currency: string;
+  code: string;
+  amount: string;
+  changes: string;
+  type: string;
+}
 
 const Home = () => {
-  const navigation = useNavigation();
+  //   const navigation = useNavigation();
+
+  const [trending, setTrending] = useState(dummyData.trendingCurrencies);
+
+  function renderHeader() {
+    const renderItem = ({item, index}: {item: PropsItem; index: number}) => (
+      <TouchableOpacity
+        style={{
+          width: 180,
+          paddingVertical: SIZES.padding,
+          paddingHorizontal: SIZES.padding,
+          marginLeft: index == 0 ? SIZES.padding : 0,
+          marginRight: SIZES.radius,
+          borderRadius: 10,
+          backgroundColor: COLORS.white,
+        }}>
+        {/* Currency */}
+        <View style={{flexDirection: 'row'}}>
+          <View>
+            <Image
+              source={item.image}
+              resizeMode="cover"
+              style={{
+                marginTop: 5,
+                width: 25,
+                height: 25,
+              }}
+            />
+          </View>
+          <View style={{marginLeft: SIZES.base}}>
+            <Text style={{...FONTS.h2}}>{item.currency}</Text>
+            <Text style={{color: COLORS.gray, ...FONTS.body3}}>
+              {item.code}
+            </Text>
+          </View>
+        </View>
+        {/* Value */}
+        <View style={{marginTop: SIZES.radius}}>
+          <Text style={{...FONTS.h2}}>${item.amount}</Text>
+          <Text
+            style={{
+              color: item.type == 'I' ? COLORS.green : COLORS.red,
+              ...FONTS.h3,
+            }}>
+            {item.changes}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    return (
+      <View style={{width: '100%', height: 290, ...styles.shadow}}>
+        <ImageBackground
+          source={images.banner}
+          resizeMode="cover"
+          style={{
+            flex: 1,
+            alignItems: 'center',
+          }}>
+          {/* Header Bar */}
+          <View
+            style={{
+              marginTop: SIZES.padding * 2,
+              width: '100%',
+              alignItems: 'flex-end',
+              paddingHorizontal: SIZES.padding,
+            }}>
+            <TouchableOpacity
+              style={{
+                width: 35,
+                height: 35,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+              onPress={() => console.log('Notification on pressed')}>
+              <Image
+                source={icons.notification_white}
+                resizeMode="contain"
+                style={{flex: 1}}
+              />
+            </TouchableOpacity>
+          </View>
+          {/* Balance */}
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{color: COLORS.white, ...FONTS.h3}}>
+              Your Portfolio Balance
+            </Text>
+            <Text
+              style={{
+                marginTop: SIZES.base,
+                fontWeight: 'bold',
+                color: COLORS.white,
+                ...FONTS.h1,
+              }}>
+              ${dummyData.portfolio.balance}
+            </Text>
+            <Text style={{color: COLORS.white, ...FONTS.body5}}>
+              {dummyData.portfolio.changes} Last 24 hours
+            </Text>
+          </View>
+          {/* Trending */}
+          <View
+            style={{
+              position: 'absolute',
+              bottom: '-30%',
+            }}>
+            <Text
+              style={{
+                marginLeft: SIZES.padding,
+                color: COLORS.white,
+                ...FONTS.h2,
+              }}>
+              Trending
+            </Text>
+            <FlatList
+              contentContainerStyle={{marginTop: SIZES.base}}
+              horizontal
+              data={trending}
+              renderItem={renderItem}
+              keyExtractor={item => `${item.id}`}
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+
+  function renderAlert() {
+    return <PriceAlert />;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CryptoDetail')}>
-        <Text>Navigate to CryptoDetail</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView>
+      <View style={{flex: 1, paddingBottom: 130}}>
+        {renderHeader()}
+        {renderAlert()}
+      </View>
+    </ScrollView>
   );
 };
 
